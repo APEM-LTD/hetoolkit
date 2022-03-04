fc6d31908f6c0b4c8b67d18ced281b60691a591e
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2022-03-04 15:25:29 +0000

    RB 04.03

--- R/predict_indices.R
+++ R/predict_indices.R
@@ -1,14 +1,16 @@
 #' Predict expected scores for macroinvertebrate indices
 #'
 #' @description
-#' The `predict_indices` function mirrors the functionality of the RICT model available on the MS Azure platform (<https://gallery.azure.ai/Experiment/RICT-package-2>). Specifically, it uses  environmental (ENV) data from Ecology Data Explorer to generate expected scores under minimally impacted reference conditions for 80 indices, plus probabilities for RIVPACS end-groups. No classification is undertaken.
+#' The `predict_indices` function mirrors the functionality of the RICT model available on the MS Azure platform (<https://gallery.azure.ai/Experiment/RICT-package-2>). Specifically, it uses  environmental (ENV) data from Ecology Data Explorer to generate expected scores under minimally impacted reference conditions for 80 indices, plus probabilities for RIVPACS end-groups. The prediction functionality applies the 'rict_predict()' function from the AquaMetrics RICT package (https://github.com/aquaMetrics/rict). No classification is undertaken.
 #'
 #' @usage
 #' predict_indices(env_data = x, save = FALSE, save_dir = getwd())
 #'
 #' @param env_data A data frame or tibble containing site-level environmental data in Environment Agency Ecology Data Explorer format (as produced by the import_env function).
+#' @param file_format Format in which env_data is supplied: "EDE" - environmental data is formatted as downloaded from the EA's Ecology Data Explorer; "RICT" - environmental data is in the RICT template format.
 #' @param save Specifies whether or not expected indices data should be saved as a rds file (for future use); Default = TRUE.
 #' @param save_dir Path to folder where expected indices data is to be saved; Default = Current working directory.
+#' @param all_indices Boolean. TRUE - Return all indices in output (default); FALSE - only returns WHPT indices (arguement passed to rict::rict_predict).
 #'
 #' @details
 #' All data validation and transformation (conversion) are done in this function using functions predefined in HelperFunctionsv1.R. Predictions are made using PredictionfunctionsV2.R.
@@ -23,36 +25,48 @@
 #' @export
 #'
 #' @examples
-#' # Generate expected scores for macroinvertebrate indices, using environmental data for site(s) of interest.
-#' # Save generated dataset as .RDS file.
+#' # Generate expected scores for macroinvertebrate indices, using environmental data downloaded from Ecology Data Explorer for site(s) of interest.
+#' # Save dataset as .RDS file.
 #' # predict_indices(env_data = env_data,
-#' #                 save = TRUE)
+#'                   file_format = "EDE",
+#'                   save = TRUE)
 
 
 predict_indices <- function(env_data,
+                            file_format = "EDE",
                             save = FALSE,
                             save_dir = getwd(),
                             all_indices = TRUE){
 
   if(file.exists(save_dir) == FALSE) {stop("Specified save directory does not exist")}
-  if(is.object(env_data) == FALSE) {stop("Environmental data file does not exist.")}
+  if(is.object(env_data) == FALSE) {stop("Environmental data file does not exist")}
   if(is.logical(save) == FALSE) {stop("Save is not logical")}
+  if(is.logical(all_indices) == FALSE){stop("all_indices must be logical")}
+  if(isTRUE((file_format == "EDE") | isTRUE(file_format == "RICT")) == FALSE)
+    {stop("file_format must be specified as EDE or RICT")}
 
 
   # Model
   model <- c("RIVPACS IV GB") # Changed in AZURE/RSTUDIO
 
-  env_data <- env_data %>% dplyr::rename(SITE = biol_site_id)
-
   # Get data
   raw.input.data <- env_data
+
+  if(isTRUE(file_format == "EDE") == TRUE){
+
+  # check substrate variables for presence of NAs
+    raw.input.data %>%
+      select(BOULDERS_COBBLES, PEBBLES_GRAVEL, SAND, SILT_CLAY) %>%
+      summarise_all(funs(sum(is.na(.))))
+
+    # replace NAs, if required
+    raw.input.data$BOULDERS_COBBLES[is.na(raw.input.data$raw.input.data)] <- 0
 
   # Add RICT Format File Columns
   raw.input.data$SPR_SEASON_ID <- 1
   raw.input.data$SUM_SEASON_ID <- 2
   raw.input.data$AUT_SEASON_ID <- 3
   raw.input.data$velocity <- NA
-  raw.input.data$Year <- NA
 
   raw.input.data$"SPR_TL2_WHPT_ASPT (ABW,DISTFAM)" <- NA
   raw.input.data$"SPR_TL2_WHPT_NTAXA (ABW,DISTFAM)" <- NA
@@ -92,6 +106,8 @@ predict_indices <- function(env_data,
                   MEAN_WIDTH = WIDTH,
                   MEAN_DEPTH = DEPTH) %>%
     dplyr::mutate(YEAR = as.integer(format(Sys.Date(), "%Y")))
+
+  }
 
   final_input_data <- data.frame(raw.input.data)
 
--- tests/testthat/test-predict_indices.R
+++ tests/testthat/test-predict_indices.R
@@ -4,12 +4,9 @@
 
 test_that("Save directory error message is functioning", {
 
-master.file <- readxl::read_excel("NDMN Ecology Master Network PROPOSED NEW FORMAT.xlsx", sheet = "Master NDMN")
-biolsites <- dplyr::filter(master.file, Area_Code == "HNL")
-biolsites <- biolsites$Biosys_Site_ID
-env <- import_env(sites = biolsites)
+  env_data <- readRDS("env_data_predict.rds")
 
-expect_error(predict_indices(env_data = env,
+expect_error(predict_indices(env_data = env_data,
                              save_dir = "hello"),
              "Specified save directory does not exist")
 
@@ -17,13 +14,48 @@ expect_error(predict_indices(env_data = env,
 
 test_that("save = logical error message is functioning", {
 
-  master.file <- readxl::read_excel("NDMN Ecology Master Network PROPOSED NEW FORMAT.xlsx", sheet = "Master NDMN")
-  biolsites <- dplyr::filter(master.file, Area_Code == "HNL")
-  biolsites <- biolsites$Biosys_Site_ID
-  env <- import_env(sites = biolsites)
+  env_data <- readRDS("env_data_predict.rds")
 
-expect_error(predict_indices(env_data = env,
+expect_error(predict_indices(env_data = env_data,
                         save = "hello"),
              "Save is not logical")
+
+})
+
+test_that("env_data exists...", {
+
+  expect_error(predict_indices(env_data = "hello"),
+               "Environmental data file does not exist")
+
+})
+
+test_that("all_indices is logical...", {
+
+  env_data <- readRDS("env_data_predict.rds")
+
+  expect_error(predict_indices(env_data = env_data,
+                               all_indices = "hello"),
+               "all_indices must be logical")
+})
+
+test_that("file_format...", {
+
+  env_data <- readRDS("env_data_predict.rds")
+
+  expect_error(predict_indices(env_data = env_data,
+                               file_format = "hello"),
+               "file_format must be specified as EDE or RICT")
+
+})
+
+
+test_that("predict_indices constructs expected output...'", {
+
+  env_data <- readRDS("env_data_predict.rds")
+
+  result <- predict_indices(env_data = env_data)
+
+  compared <- readRDS("predict_data_output.rds")
+  expect_equivalent(result, compared)
 
 })
