de9e82c10da934dc2e06b49c5f275b1de1677a74
IGordon-APEM <i.gordon@apemltd.co.uk>
2022-12-07 10:29:47 +0000

    IG 07/12/22 Changes re-made to import_inv

--- R/import_inv.R
+++ R/import_inv.R
@@ -61,9 +61,8 @@ import_inv <- function(source = "parquet",
   if(file.exists(save_dir) == FALSE) {stop("Specified save directory does not exist")}
   if(is.logical(save) == FALSE) {stop("Save is not logical")}
   if(is.logical(save_dwnld) == FALSE) {stop("Save_dwnld is not logical")}
-  if(is.null(source) == FALSE && source %in% c("parquet", "csv") == FALSE){
-    if (grepl(source,"\\.csv$|\\.rds$") == FALSE)
-    {stop("Download format must be parquet or csv, or a valid filepath must be specified (.csv or .rds)")}}
+  if(is.null(source) == FALSE && source %in% c("parquet", "csv") == FALSE && grepl(source,"\\.csv$|\\.rds$") == FALSE)
+    {stop("Download format must be parquet or csv, or a valid filepath must be specified (.csv or .rds)")}
 
   if(is.null(biol_dir) == FALSE)
     {warning("In function import_inv, biol_dir argument deprecated. File paths can be specified using source.")}
